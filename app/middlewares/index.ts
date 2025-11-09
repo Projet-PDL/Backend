1751432c0427305b26a9da1a76d3b87617ce1854
Sofiane <ls_yekene@esi.dz>
2025-11-09 17:24:10 +0100

    Refactor middleware registration to be asynchronous and update Prisma import path

--- app/middlewares/index.ts
+++ app/middlewares/index.ts
@@ -3,16 +3,18 @@ import fastifyCors from '@fastify/cors';
 import errorHandler from './errorHandlerMiddleware';
 import requestLoggerMiddleware from './RequestLoggerMiddleware';
 
-const registerMiddlewares = (fastify: FastifyInstance) => {
-
+const registerMiddlewares = async (fastify: FastifyInstance) => {
+  // Register CORS first
+  await fastify.register(fastifyCors, {
+    origin: '*',
+    credentials: true, // Allow credentials (cookies, authorization headers)
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   
+  // Then register other middlewares
   requestLoggerMiddleware(fastify);
   errorHandler(fastify);
-  fastify.register(fastifyCors, {
-    origin: '*',
-  });
-
-
 };
 
 export default registerMiddlewares;
