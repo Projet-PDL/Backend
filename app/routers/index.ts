545cb665ab4d5c1ecd32e3da685836abefe6a89c
Elisabeth <yah.gnimassou@gmail.com>
2025-11-09 16:33:29 +0100

    feat(backend): cvs management

--- app/routers/index.ts
+++ app/routers/index.ts
@@ -2,12 +2,15 @@
 import exampleRoutes from './example.routes';
 import { FastifyInstance, FastifyPluginOptions } from 'fastify';
 import authRoutes from "./auth/auth.router"
+import cvRoutes from "./cv/cv.router";
 
 const registerRoutes = (fastify: FastifyInstance) => {
   // Register example routes with a prefix
   fastify.register(exampleRoutes, { prefix: '/example' });
   
   fastify.register(authRoutes, { prefix: '/auth' });
+
+  fastify.register(cvRoutes, { prefix: '/cvs' });
 
 };
 
